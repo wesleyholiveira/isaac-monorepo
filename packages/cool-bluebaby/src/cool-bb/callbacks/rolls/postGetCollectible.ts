a03420b6d3a38413aaba98098a8e48b38ad2122c
Wesley Oliveira <wesleyholiveir@gmail.com>
2023-08-14 02:27:55 -0300

    fix(cool-bb): corrigido condição que impede do DIRTY MIND ser rerollado no coop

--- packages/cool-bluebaby/src/cool-bb/callbacks/rolls/postGetCollectible.ts
+++ packages/cool-bluebaby/src/cool-bb/callbacks/rolls/postGetCollectible.ts
@@ -1,18 +1,20 @@
 import { CollectibleType, ItemPoolType, ModCallback } from "isaac-typescript-definitions";
 import { CoolBBState } from "../../states/cool-bb.state";
 import { PlayerTypeCustom } from "../../../@shared/enums/PlayerTypeCustom";
+import { getPlayers } from "isaacscript-common";
 
 export function postGetCollectible(mod: Mod): void {
     mod.AddCallback(ModCallback.POST_GET_COLLECTIBLE, getCollectible);
 }
 
 function getCollectible(collectibleType: CollectibleType, itemPoolType: ItemPoolType, decrease: boolean, seed: Seed) {
-    const player = Isaac.GetPlayer();
+    const players = getPlayers().filter((p) =>
+            p.GetPlayerType() === PlayerTypeCustom.COOL_BB &&
+            !p.HasCollectible(CollectibleType.DIRTY_MIND
+        )
+    );
 
-    if (
-        player.GetPlayerType() === PlayerTypeCustom.COOL_BB &&
-        !player.HasCollectible(CollectibleType.DIRTY_MIND)
-    ) {
+    if (players.length > 0) {
         CoolBBState.persist.removedDirtyMind = true;
         return CollectibleType.DIRTY_MIND;
     }
